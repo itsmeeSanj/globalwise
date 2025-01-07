33158b4c1e82763f3acd1b5613f4c6889d40732d
itsmeeSanj <itsmeesanj@gmail.com>
2025-01-07 14:53:53 -0500

    add:city name in map with lng and lat

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
+import { useCities } from "../contexts/CitiesContext";
 import styles from "./Map.module.css";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [mapPosition, setMapPosition] = React.useState([40, 0]);
+
+  const { cities } = useCities();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
@@ -25,11 +28,20 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {cities.map((city, index) => {
+          return (
+            <Marker
+              position={[city.position.lat, city.position.lng]}
+              key={index}
+            >
+              <Popup>
+                <span>{city.emoji}</span>
+                <span>{city.cityName}</span>
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
