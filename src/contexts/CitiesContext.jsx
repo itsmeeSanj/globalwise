fdc8b49da6e84be34d6efb4a996dee1722d00db5
itsmeeSanj <itsmeesanj@gmail.com>
2025-01-11 15:47:01 -0500

    change absolute name

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,25 +1,66 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useReducer } from "react";
 
 const BASE_URL = "http://localhost:9000";
 // 1.
 const CitiesContext = createContext();
 
+const initialState = {
+  cities: [],
+  isLoading: false,
+  curCity: {},
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+
+    case "cities/loaded": // should be more like event
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+
+    case "cities/created":
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case "cities/deleted":
+      return {
+        ...state,
+        isLoading: false,
+      };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
 function CitiesProvider({ children }) {
-  const [cities, setCities] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [curCity, setCurCity] = React.useState({});
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { cities, isLoading, curCity } = state;
+
+  // const [cities, setCities] = React.useState([]);
+  // const [isLoading, setIsLoading] = React.useState(false);
+  // const [curCity, setCurCity] = React.useState({});
 
   React.useEffect(function () {
     async function dataFetech() {
+      dispatch({
+        type: "loading",
+      });
+
       try {
-        setIsLoading(true);
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCities(data);
+        // setCities(data);
       } catch (error) {
         throw new Error("Something is wrong :(", error);
       } finally {
-        setIsLoading(false);
+        // setIsLoading(false);
       }
     }
     dataFetech();
@@ -99,7 +140,7 @@ function useCities() {
   const value = useContext(CitiesContext);
 
   if (value === undefined)
-    throw new Error("Cities contex  was used outside of the citiesProvider");
+    throw new Error("Cities contex was used outside of the citiesProvider");
 
   return value;
 }
