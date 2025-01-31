48c7880bb4205695a20ca88457e07d535e073219
itsmeeSanj <itsmeesanj@gmail.com>
2025-01-31 15:54:28 -0500

    add:lazy-loading (split bundle Js FIle from server)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import { lazy } from "react";
+
 import { CitiesProvider } from "./contexts/CitiesContext";
-import Pricing from "./pages/Pricing";
-import Product from "./pages/Product";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Homepage from "./pages/Homepage";
-import Login from "./pages/Login";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import CityList from "./Components/CityList";
 import CountriesList from "./Components/CountriesList";
 import City from "./Components/City";
 import Form from "./Components/Form";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+
+// import Login from "./pages/Login";
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
+
+// dist/assets/index-CMeyRBQW.css   30.23 kB │ gzip:   5.04 kB
+// dist/assets/index-DczWp8Xe.js   564.16 kB │ gzip: 166.44 kB
 
 function App() {
   return (
